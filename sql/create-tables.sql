779984df89f9fb3be5fc0b627d04bf436f6d661e
Muhammad Rehman Rabbani <rehmanrabbani01@gmail.com>
2025-01-27 19:20:50 +0100

    Add create tables script

---
+++ sql/create-tables.sql
@@ -0,0 +1,39 @@
+-- 1- User:
+-- Stores customer/user data for authentication and profile management.
+CREATE TABLE User (
+    user_id INT PRIMARY KEY,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(MAX) NOT NULL,
+    address VARCHAR(255),
+    created_at DATETIME DEFAULT GETDATE()
+);
+
+-- 2- Product:
+-- Stores product details. Image URL stores links to images in Azure Blob Storage.
+CREATE TABLE Product (
+    product_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(MAX),
+    price DECIMAL(10, 2) NOT NULL,
+    image_url VARCHAR(255),
+    category_id INT NOT NULL FOREIGN KEY REFERENCES Category(category_id)
+);
+
+-- 3- Category:
+-- Stores product category information to normalize the Products table.
+CREATE TABLE Category (
+    category_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+-- 4- Inventory:
+-- Tracks stock levels and supplier information.
+CREATE TABLE Inventory (
+    inventory_id INT PRIMARY KEY,
+    stock_quantity INT NOT NULL,
+    stock_level VARCHAR(50) NOT NULL, -- High, Medium, Low, Out of Stock
+    supplier VARCHAR(50),
+    product_id INT NOT NULL FOREIGN KEY REFERENCES Product(product_id)
+);
