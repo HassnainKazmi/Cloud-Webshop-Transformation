761fb27f19775f007e294c562cbb12f0f6cccc0f
usman <muhammad.ghani@stud.fra-uas.de>
2025-02-01 17:03:58 +0100

    Added migrations folder and User and Inventory model

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from flask_migrate import Migrate
+import os
+load_dotenv()
+
 import app
 
-app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}'
+app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app.app)
+Migrate(app=app.app, db=db)
 
 with app.app.app_context():
     print('Creating database')
+    # db.drop_all()
     db.create_all()
