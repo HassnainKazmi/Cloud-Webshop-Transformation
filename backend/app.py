437375a539edf3483d47eb2d55c91194b1867ca3
usman <muhammad.ghani@stud.fra-uas.de>
2025-02-01 17:03:58 +0100

    Added migrations folder and User and Inventory model

--- backend/app.py
+++ backend/app.py
@@ -7,9 +7,11 @@ api = Api(app)
 import controller.Products
 import controller.Category
 
+# Products endpoints
 api.add_resource(controller.Products.Products, '/api/products/', endpoint='products')
 api.add_resource(controller.Products.ProductOperations, '/api/product/<int:id>')
 
+# Categories endpoints
 api.add_resource(controller.Category.Categories, '/api/categories/', endpoint='categories')
 
 @app.route('/')
--- backend/models/models.py
+++ backend/models/models.py
@@ -1,6 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 import database
 
+class StockLevel(enum.Enum):
+    LOW = 'LOW'
+    MEDIUM = 'MEDIUM'
+    HIGH = 'HIGH'
+    OUT_OF_STOCK = 'OUT OF STOCK'
+
+class User(database.db.Model):
+    __tablename__ = 'user'
+    id = database.db.Column(database.db.Integer, primary_key=True)
+    first_name = database.db.Column(database.db.String(30), nullable=False)
+    last_name = database.db.Column(database.db.String(30), nullable=False)
+    email = database.db.Column(database.db.String(70), nullable=False)
+    address = database.db.Column(database.db.String(200), nullable=False)
 
 class Category(database.db.Model):
     __tablename__ = 'category'
@@ -13,7 +27,14 @@ class Product(database.db.Model):
     id = database.db.Column(database.db.Integer, primary_key=True)
     name = database.db.Column(database.db.String(80), unique=True, nullable=False)
     quantity = database.db.Column(database.db.Integer)
-    category_id = database.db.Column(database.db.Integer, database.db.ForeignKey('category.id'))
+    category_id = database.db.Column(database.db.Integer, database.db.ForeignKey('category.id'), nullable=False)
 
     def __repr__(self):
         return f'id: {self.id}\tName: {self.name}'
+
+class Inventory(database.db.Model):
+    __tablename__ = 'inventory'
+    id = database.db.Column(database.db.Integer, primary_key=True)
+    product_id = database.db.Column(database.db.Integer, database.db.ForeignKey('product.id'), nullable=False)
+    stock_quantity = database.db.Column(database.db.Integer, nullable=False)
+    stock_level = database.db.Column(database.db.Enum(StockLevel), nullable=False)
