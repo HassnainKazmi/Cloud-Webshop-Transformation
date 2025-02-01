9e93fa88f9152b4314570c440e85eff8be0a44d9
usman <muhammad.ghani@stud.fra-uas.de>
2025-02-01 18:55:40 +0100

    refined CRUD endpoints for products

--- backend/controller/Products.py
+++ backend/controller/Products.py
@@ -1,33 +1,43 @@
 from flask_restful import Resource, marshal_with, reqparse, abort
 
 import database
-from models.models import Product
+from models.models import Product, Category
 from utils.fields import product_fields
 
 product_args = reqparse.RequestParser()
 product_args.add_argument('name', type=str, required=True, help='Name is required')
 product_args.add_argument('quantity', type=int, required=True, help='Quantity is required')
+product_args.add_argument('category_id', type=int, required=False)
 
 class Products(Resource):
     @marshal_with(product_fields)
     def get(self):
-        products = Product.query.all()
-        return products
+        return self._get_products_list()
 
     @marshal_with(product_fields)
     def post(self):
         args = product_args.parse_args()
-        new_product = Product(name=args['name'], quantity=args['quantity'])
+        category = Category().query.filter_by(id=args['category_id']).first()
+        new_product = Product(name=args['name'], quantity=args['quantity'], category=category)
         database.db.session.add(new_product)
-        database.db.session.commit()
-        all_products = Product.query.all()
-        return all_products, 201
+        return self._get_products_list(), 201
+
+    def _get_products_list(self):
+        products = Product.query.all()
+        products_list = []
+        for product in products:
+            product_dict = product.__dict__
+            product_dict['category_name'] = product.category.name
+            products_list.append(product_dict)
+        return products_list
 
 
 class ProductOperations(Resource):
     @marshal_with(product_fields)
     def patch(self, id):
         args = product_args.parse_args()
+        if args.get('category_id') is not None:
+            abort(400, message=f'category id update is not allowed!')
         product_to_update = Product.query.filter_by(id=id).first()
         if not product_to_update:
             abort(404, message='Product not found!')
