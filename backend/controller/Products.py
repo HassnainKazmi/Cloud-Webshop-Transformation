14e2da7d49d189a3484f36fcf9808a73cbf4ea4f
usman <muhammad.ghani@stud.fra-uas.de>
2025-02-01 15:43:26 +0100

    Added category model, implemented one to many relationship between categories and products and added get all categories endpoint

--- backend/controller/Products.py
+++ backend/controller/Products.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource, marshal_with, reqparse, abort
 
 import database
-from models.Product import ProductModel
+from models.models import Product
 from utils.fields import product_fields
 
 product_args = reqparse.RequestParser()
@@ -11,16 +11,16 @@ product_args.add_argument('quantity', type=int, required=True, help='Quantity is
 class Products(Resource):
     @marshal_with(product_fields)
     def get(self):
-        products = ProductModel.query.all()
+        products = Product.query.all()
         return products
 
     @marshal_with(product_fields)
     def post(self):
         args = product_args.parse_args()
-        new_product = ProductModel(name=args['name'], quantity=args['quantity'])
+        new_product = Product(name=args['name'], quantity=args['quantity'])
         database.db.session.add(new_product)
         database.db.session.commit()
-        all_products = ProductModel.query.all()
+        all_products = Product.query.all()
         return all_products, 201
 
 
@@ -28,7 +28,7 @@ class ProductOperations(Resource):
     @marshal_with(product_fields)
     def patch(self, id):
         args = product_args.parse_args()
-        product_to_update = ProductModel.query.filter_by(id=id).first()
+        product_to_update = Product.query.filter_by(id=id).first()
         if not product_to_update:
             abort(404, message='Product not found!')
         product_to_update.name = args['name']
@@ -38,10 +38,10 @@ class ProductOperations(Resource):
 
     @marshal_with(product_fields)
     def delete(self, id):
-        product_to_delete = ProductModel.query.filter_by(id=id).first()
+        product_to_delete = Product.query.filter_by(id=id).first()
         if not product_to_delete:
             abort(404, message='Product not found!')
         database.db.session.delete(product_to_delete)
         database.db.session.commit()
-        all_products = ProductModel.query.all()
+        all_products = Product.query.all()
         return all_products, 200
--- backend/database.py
+++ backend/database.py
@@ -5,4 +5,5 @@ app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app.app)
 
 with app.app.app_context():
+    print('Creating database')
     db.create_all()
