4eda647bf9f926897178ca0d5aeb61be92cf1184
usman <muhammad.ghani@stud.fra-uas.de>
2025-02-04 16:15:02 +0100

    Added get all order and update order endpoint, also added nested marshall

--- backend/controller/Order.py
+++ backend/controller/Order.py
@@ -4,21 +4,55 @@ import database
 from models.models import Order as OrderModel, User, Product, OrderItem, Inventory
 
 from utils.fields import order_fields
-from utils.order_request_arguments import post_order_arguments
+from utils.order_request_arguments import post_order_arguments, patch_order_arguments
 from utils.utils import calculate_stock_level
+from utils.enums import OrderStatus
 
 
 class Order(Resource):
     @marshal_with(order_fields)
     def get(self, id):
         """Get order with the id."""
-        orders = OrderModel.query.filter_by(id=id).first()
-        if not orders:
+        order = OrderModel.query.filter_by(id=id).first()
+        if not order:
             abort(404, message="No order found with the provided id")
-        return orders, 200
+        order_dict = order.__dict__
+        order_dict["products"] = [o.product_info for o in order.order_items]
+        return order_dict, 200
+
+    @marshal_with(order_fields)
+    def patch(self, id):
+        """Updates order's status"""
+        args = patch_order_arguments.parse_args()
+        order = OrderModel.query.filter_by(id=id).first_or_404()
+        allowed_status = OrderStatus.list()
+        new_status = args["status"]
+        if new_status not in allowed_status:
+            abort(400, message="Passed value of new status not allowed!")
+        # if order.status != OrderStatus.IN_PROCESS:
+        #     abort(400, message='')
+        order.status = new_status
+        database.db.session.commit()
+        order_dict = order.__dict__
+        order_dict["products"] = [o.product_info for o in order.order_items]
+        print(order_dict)
+        return order_dict, 200
 
 
 class OrderOperations(Resource):
+    @marshal_with(order_fields)
+    def get(self):
+        """Get all orders."""
+        orders = OrderModel.query.all()
+        if not orders:
+            abort(404, message="No order found with the provided id")
+        order_list = []
+        for order in list(orders):
+            order_dict = order.__dict__
+            order_dict["products"] = [o.product_info for o in order.order_items]
+            order_list.append(order_dict)
+        return order_list, 200
+
     def post(self):
         """Create order with the provided json."""
         args = post_order_arguments.parse_args()
