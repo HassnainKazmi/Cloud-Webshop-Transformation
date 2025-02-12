bf2e2c54c8c98de710d9b36b47af760061767abc
usman <muhammad.ghani@stud.fra-uas.de>
2025-02-12 22:20:32 +0100

    Cleaned up migrations and added sql init script for docker containers

--- backend/models/models.py
+++ backend/models/models.py
@@ -14,13 +14,6 @@ class User(database.db.Model):
     orders = database.db.relationship("Order", backref="user_info")
 
 
-class Category(database.db.Model):
-    __tablename__ = "category"
-    id = database.db.Column(database.db.Integer, primary_key=True)
-    name = database.db.Column(database.db.String(80), unique=True, nullable=False)
-    products = database.db.relationship("Product", backref="category")
-
-
 class Product(database.db.Model):
     __tablename__ = "product"
     id = database.db.Column(database.db.Integer, primary_key=True)
@@ -34,6 +27,13 @@ class Product(database.db.Model):
 
     def __repr__(self):
         return f"id: {self.id}\tName: {self.name}"
+
+
+class Category(database.db.Model):
+    __tablename__ = "category"
+    id = database.db.Column(database.db.Integer, primary_key=True)
+    name = database.db.Column(database.db.String(80), unique=True, nullable=False)
+    products = database.db.relationship("Product", backref="category")
 
 
 class Inventory(database.db.Model):
