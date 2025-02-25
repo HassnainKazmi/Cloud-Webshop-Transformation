fb4a5a597203afd87c4fdadfe10d747031f8bffe
usman <muhammad.ghani@stud.fra-uas.de>
2025-02-25 01:17:23 +0100

    azure blob image storage

--- backend/controller/Products.py
+++ backend/controller/Products.py
@@ -1,5 +1,7 @@
+import os
 from flask_restful import Resource, marshal_with, abort
 from flask import request
+from azure.storage.blob import BlobClient, ContentSettings
 
 import database
 from models.models import Product, Category, Inventory
@@ -7,6 +9,11 @@ from models.models import Product, Category, Inventory
 from utils.fields import product_fields
 from utils.product_request_arguments import post_product_args, patch_product_args
 from utils.utils import calculate_stock_level
+
+
+AZURE_STORAGE_ACCOUNT_NAME = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+AZURE_CONTAINER_NAME = os.getenv("AZURE_CONTAINER_NAME")
+AZURE_SAS_TOKEN = os.getenv("AZURE_SAS_TOKEN")
 
 
 class Products(Resource):
@@ -18,9 +25,6 @@ class Products(Resource):
     def post(self):
         args = post_product_args.parse_args()
         image = request.files["image"]
-        print(image)
-        if image:
-            print(image)
         category = Category().query.filter_by(id=args["category_id"]).first()
         new_product = Product(
             name=args["name"],
@@ -29,6 +33,22 @@ class Products(Resource):
         )
         database.db.session.add(new_product)
         database.db.session.commit()
+        if image:
+            original_filename, file_extension = os.path.splitext(image.filename)
+            new_filename = f"{original_filename}_{new_product.id}{file_extension}"
+            image.filename = new_filename
+            blob_url = f"https://{AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net/{AZURE_CONTAINER_NAME}/{image.filename}{AZURE_SAS_TOKEN}"
+            content_type = (
+                "image/jpeg"
+                if file_extension.lower() in [".jpg", ".jpeg"]
+                else "image/png"
+            )
+            blob_client = BlobClient.from_blob_url(blob_url)
+            blob_client.upload_blob(
+                image,
+                overwrite=True,
+                content_settings=ContentSettings(content_type=content_type),
+            )
         # add the product to inventory along with its quantity
         inventory_item = Inventory(
             product_id=new_product.id,
